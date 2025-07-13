a9b1ec48f7f40ddc18d78d7f94bb481d6fe0db87
seoyeong-y <ysy2329@gmail.com>
2025-07-13 16:07:18 +0900

    Feat: 추천 화면 기본 구조 생성

---
+++ app/src/main/java/com/example/baba/ui/recommendation/RecommendatinoScreen.kt
@@ -0,0 +1,32 @@
+package com.example.baba.ui.recommendation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.baba.ui.theme.TextBlack
+
+@Composable
+fun RecommendationScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "추천해요 화면",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = TextBlack
+        )
+    }
+}
